3a8eab7a7311cd9f2528633fc81988c19834c319
Ricardo Quiroga <l2radamanthys@gmail.com>
2013-10-23 13:03:27 -0300

    falta actualizar lso cambios en la bd para personas

---
+++ application/config/form_validation.php
@@ -0,0 +1,14 @@
+<?php
+
+$config = array(
+    'persona' => array(
+        array('field' => 'nombre_pers', 'label' => 'Nombre', 'rules' => 'required'),
+        array('field' => 'apellido_pers', 'label' =>'Apellido', 'rules' => 'required'),
+        array('field' => 'nro_dni_pers', 'label' =>'Numero de Documento', 'rules' => 'numeric'),
+        
+    ), 
+    
+    
+    
+);
+
---
+++ application/views/personas/registrar_success.php
@@ -0,0 +1,26 @@
+<script type="text/javascript">
+    //ejecutar al inicio
+    $(function() {
+        $('#button').button();   
+    });
+</script>
+
+
+<section class="one-section">
+        
+    <h1><?=$title;?></h1>
+    
+    <div style="width: 500px; margin: 0 auto;">
+        <div class="notify-success">
+            El cliente fue registrado exitosamente.
+        </div>
+        
+        <br />
+        <p style="text-align:center">
+            <a href="" id="botton">Continuar</a>            
+        </p>
+    
+    </div>
+    
+    
+</section>
