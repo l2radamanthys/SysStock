00f876c6c69cb34047751d0d1b0e29db04958c26
Ricardo Quiroga <l2radamanthys@gmail.com>
2013-10-24 11:41:27 -0300

    correccion errores campos prefijo tabla personas

---
+++ application/helpers/utils.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * carga los datos de post, de los campos solicitados
+ */
+function create_post_dict($keys) {
+    $data = array();
+    $ci =& get_instance();
+    foreach ($keys as $key) {
+        $data[$key] = $ci->input->post($key);
+    }    
+    return $data;
+    
+}
